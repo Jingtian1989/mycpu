f9b565b073d761210443e79cca1803fcd3c25d02
jingtian1989 <zjt415991957@qq.com>
2014-01-19 13:12:59 +0800

    Update

--- testbench/cpu/if/if_stage_tb.v
+++ testbench/cpu/if/if_stage_tb.v
@@ -52,17 +52,18 @@ module if_stage_tb();
 		.bus_rw(bus_rw), .bus_wr_data(bus_wr_data),
 
 		.clk(clk), .reset(reset), .stall(stall), .flush(flush),
-		
-		.new_pc(new_pc), 
+
+		.new_pc(new_pc),
 		.br_taken(br_taken), .br_addr(br_addr),
-		.if_pc(if_pc), .if_insn(if_insn), 
+		.if_pc(if_pc), .if_insn(if_insn),
 		.if_en_(if_en_)
 		);
 
+
 	initial begin
 		#0 begin
-			spm_rd_data 	<= `WORD_DATA_W'h0;
-			bus_rd_data 	<= `WORD_DATA_W'h0;
+			spm_rd_data <= `WORD_DATA_W'h0;
+			bus_rd_data <= `WORD_DATA_W'h0;
 			bus_rdy_ 	<= `DISABLE_;
 			bus_grnt_ 	<= `DISABLE_;
 
@@ -83,11 +84,11 @@ module if_stage_tb();
 
 		#1 begin
 			reset 		<=`RESET_DISABLE;
-			spm_rd_data 	<=`WORD_DATA_W'h99;
-			bus_rd_data 	<=`WORD_DATA_W'h98;
+			spm_rd_data <=`WORD_DATA_W'h99;
+			bus_rd_data <=`WORD_DATA_W'h98;
 			new_pc 		<=`WORD_ADDR_W'h97;
 			br_taken 	<=`DISABLE;
-			br_addr 	<=`WORD_ADDR_W'h96;	
+			br_addr 	<=`WORD_ADDR_W'h96;
 		end
 
 		#1 begin
@@ -100,6 +101,5 @@ module if_stage_tb();
 	end
 
 endmodule
-
 
 
